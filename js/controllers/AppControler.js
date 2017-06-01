21dfd7fced6dbd2d3510f6d431e80fd1a367d4ed
Donny <1021001390@qq.com>
2017-06-01 16:06:25 +0800

    fix resolution bug

--- js/controllers/AppControler.js
+++ js/controllers/AppControler.js
@@ -11,21 +11,18 @@
             };
 
             var url = 'http://192.168.250.45:6080/arcgis/rest/services/ThemeMap/MapServer';
-            // var extent = [111.63519615524577, 32.373406903804636, 111.72406902381353, 32.413810151683634];
-            var extent = [12426884.777, 3812409.2678, 12433705.749, 3815674.2327];
+            var extent = [111.63519615524577, 32.373406903804636, 111.72406902381353, 32.413810151683634];
             var projection = new ol.proj.Projection({
-                code: 'EPSG:4490',
-                // units: 'm'
+                code: 'EPSG:4490'
             });
             var map = new ol.Map({
                 target: 'map',
                 layers: [new ol.layer.Image()],
                 view: new ol.View({
                     center: [(extent[0] + extent[2]) / 2, (extent[1] + extent[3]) / 2],
-                    // zoom: 15,
                     extent: extent,
-                    // projection: projection,
-                    resolution: 96
+                    projection: projection,
+                    resolution: 0.0001
                 })
             });
 
